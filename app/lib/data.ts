2a28d3a0aa391f70fdfc3413aaa1eeaf94000526
marcos-reynoso <marcos_reynoso95@hotmail.com>
2025-09-03 15:22:50 -0300

    feat: Implement dashboard and invoice management features
    - Add ESLint configuration for code quality.
    - Create loading state component for dashboard.
    - Develop main dashboard page with revenue chart and latest invoices.
    - Implement 404 Not Found page for invoice editing.
    - Create invoice editing page with form and breadcrumbs.
    - Add invoice creation page with form and breadcrumbs.
    - Implement error handling component for invoices.
    - Add server-side actions for creating, updating, and deleting invoices.
    - Implement authentication logic with NextAuth and credentials provider.
    - Create login page with form and branding.
    - Configure authentication settings and middleware for protected routes.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -9,19 +9,15 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const sql = postgres(process.env.POSTGRES_URL || process.env.DATABASE_URL!, { ssl: 'require' });
 
 export async function fetchRevenue() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+
 
     return data;
   } catch (error) {
@@ -33,12 +29,11 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   try {
     const data = await sql<LatestInvoiceRaw[]>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
-
+  SELECT invoices.amount, customers.name, customers.image_url, customers.email
+  FROM invoices
+  JOIN customers ON invoices.customer_id = customers.id
+  ORDER BY invoices.date DESC
+  LIMIT 5`;
     const latestInvoices = data.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
